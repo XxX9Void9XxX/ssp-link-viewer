0d916d42b13ab970da0b2c93fbe16a6ee0df9c00
XxX9Void9XxX <brooksm@carbonschools.org>
2026-03-12 17:59:54 -0600

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -8,7 +8,6 @@ export default async function handler(req, res) {
     const response = await fetch(url, {
       headers: { "User-Agent": "Mozilla/5.0" }
     });
-
     const text = await response.text();
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).send(text);
